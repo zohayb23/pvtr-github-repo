1c3913456c563ab05b12e439b4c532b5c4d72e08
Eddie Knight <knight@linux.com>
2024-12-15 12:35:08 -0600

    fix: changing to match baseline ID changes (#7)

--- armory/ac-06.go
+++ armory/ac-06.go
@@ -5,21 +5,21 @@ import (
 	"github.com/privateerproj/privateer-sdk/utils"
 )
 
-func AC_07() (strikeName string, result raidengine.StrikeResult) {
-	strikeName = "AC_07"
+func AC_06() (strikeName string, result raidengine.StrikeResult) {
+	strikeName = "AC_06"
 	result = raidengine.StrikeResult{
 		Passed:      false,
 		Description: "The project’s version control system MUST require multi-factor authentication that does not include SMS for users when modifying the project repository settings or accessing sensitive data.",
-		ControlID:   "OSPS-AC-07",
+		ControlID:   "OSPS-AC-06",
 		Movements:   make(map[string]raidengine.MovementResult),
 	}
 
-	result.ExecuteMovement(AC_07_T01)
+	result.ExecuteMovement(AC_06_T01)
 
 	return
 }
 
-func AC_07_T01() (moveResult raidengine.MovementResult) {
+func AC_06_T01() (moveResult raidengine.MovementResult) {
 	moveResult = raidengine.MovementResult{
 		Description: "This movement is still under construction",
 		Function:    utils.CallerPath(0),
