b7f4be8560a0633ab1fe2e855ab5d94cc874dcd4
Eddie Knight <knight@linux.com>
2025-01-03 15:01:20 -0600

    Added DO-07 (#33)
    Signed-off-by: Eddie Knight <knight@linux.com>

--- armory/do-07.go
+++ armory/do-07.go
@@ -13,17 +13,41 @@ func DO_07() (string, pluginkit.TestSetResult) {
 	}
 
 	result.ExecuteTest(DO_07_T01)
+	if result.Tests["DO_07_T01"].Passed {
+		result.ExecuteTest(DO_07_T02)
+	}
 
 	return "DO_07", result
 }
 
-// TODO
 func DO_07_T01() pluginkit.TestResult {
+	guideLocation := Data.Rest().Insights.Repository.Security.Assessments.Self.Evidence
+	found := guideLocation != ""
+
 	testResult := pluginkit.TestResult{
-		Description: "This movement is still under construction",
+		Description: "Determine whether the project's Security Insights data specifies a self assessment location.",
 		Function:    utils.CallerPath(0),
+		Passed:      found,
+		Message:     "Self-assessment location specified in Security Insights: %t",
+		Value:       guideLocation,
 	}
 
-	// TODO: Use this section to write a single step or test that contributes to DO_07
+	return testResult
+}
+
+func DO_07_T02() pluginkit.TestResult {
+	_, err := makeApiCall(Data.Rest().Insights.Repository.Security.Assessments.Self.Evidence, false)
+
+	testResult := pluginkit.TestResult{
+		Description: "Verifying that an artifact exists at the location specified for the self assessment.",
+		Function:    utils.CallerPath(0),
+		Passed:      err == nil,
+		Message:     "URL for Self Assessment can be reached: %t",
+	}
+
+	if err != nil {
+		testResult.Value = err
+	}
+
 	return testResult
 }
