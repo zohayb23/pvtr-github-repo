96223ec96fc6c6231e74bd1b9319284eed9a84bb
Eddie Knight <knight@linux.com>
2025-01-07 07:09:30 -0600

    feat: Added BR_07 (#38)
    * feat: Added BR_07
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Added BR_07 to dev list
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    ---------
    
    Signed-off-by: Eddie Knight <knight@linux.com>

--- armory/br-07.go
+++ armory/br-07.go
@@ -1,6 +1,7 @@
 package armory
 
 import (
+	"github.com/ossf/si-tooling/v2/si"
 	"github.com/privateerproj/privateer-sdk/pluginkit"
 	"github.com/privateerproj/privateer-sdk/utils"
 )
@@ -13,16 +14,29 @@ func BR_07() (string, pluginkit.TestSetResult) {
 	}
 
 	result.ExecuteTest(BR_07_T01)
+	// TODO: Ensure the identified attestation exists
 
 	return "BR_07", result
 }
 
 func BR_07_T01() pluginkit.TestResult {
 	testResult := pluginkit.TestResult{
-		Description: "This movement is still under construction",
+		Description: "Check Security Insights Release Attestations for SLSA predicate-uri.",
 		Function:    utils.CallerPath(0),
 	}
 
-	// TODO: Use this section to write a single step or test that contributes to BR_01
+	attestations := Data.Rest().Insights.Repository.Release.Attestations
+
+	var value []si.Attestation
+
+	for _, attestation := range attestations {
+		if attestation.PredicateURI == "https://slsa.dev/provenance/v1" {
+			testResult.Passed = true
+			testResult.Message = "Attestsation with SLSA predicate type."
+			testResult.Value = attestation
+			value = append(value, attestation)
+		}
+	}
+	testResult.Value = value
 	return testResult
 }
