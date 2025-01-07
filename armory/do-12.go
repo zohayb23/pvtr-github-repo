ebe4eff33d72264f5ba0fb26a8d11248c3b09dc8
Eddie Knight <knight@linux.com>
2025-01-07 15:45:15 -0600

    feat: Added DO_12 (#39)
    Signed-off-by: Eddie Knight <knight@linux.com>

--- armory/do-12.go
+++ armory/do-12.go
@@ -1,6 +1,8 @@
 package armory
 
 import (
+	"fmt"
+
 	"github.com/privateerproj/privateer-sdk/pluginkit"
 	"github.com/privateerproj/privateer-sdk/utils"
 )
@@ -14,16 +16,38 @@ func DO_12() (string, pluginkit.TestSetResult) {
 
 	result.ExecuteTest(DO_12_T01)
 
+	if result.Tests["DO_12_T01"].Passed {
+		result.ExecuteTest(DO_12_T02)
+	}
+
 	return "DO_12", result
 }
 
-// TODO
 func DO_12_T01() pluginkit.TestResult {
+
+	guideLocation := Data.Rest().Insights.Project.Documentation.SignatureVerification
+
 	testResult := pluginkit.TestResult{
-		Description: "This movement is still under construction",
+		Description: "Ensure the project's Security Insights data specifies a signature verification guide location.",
 		Function:    utils.CallerPath(0),
+		Passed:      guideLocation != "",
+		Message:     fmt.Sprintf("Signature verification guide location specified in Security Insights: %v", guideLocation != ""),
+		Value:       guideLocation,
 	}
 
-	// TODO: Use this section to write a single step or test that contributes to DO_12
+	return testResult
+}
+
+func DO_12_T02() pluginkit.TestResult {
+
+	_, err := makeApiCall(Data.Rest().Insights.Project.Documentation.SignatureVerification, true)
+
+	testResult := pluginkit.TestResult{
+		Description: "Check if the signature verification guide is accessible via HTTP.",
+		Function:    utils.CallerPath(0),
+		Passed:      err == nil,
+		Message:     fmt.Sprintf("Signature verification guide is accessible via HTTP: %v", err == nil),
+		Value:       Data.Rest().Insights.Project.Documentation.SignatureVerification,
+	}
 	return testResult
 }
