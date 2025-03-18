b71799b2e543448e075c353d0e37a325fba1c865
Eddie Knight <knight@linux.com>
2025-03-17 22:34:53 -0500

    Added assesment for OSPS_QA_03
    Signed-off-by: Eddie Knight <knight@linux.com>

--- evaluation_plans/osps/quality/steps.go
+++ evaluation_plans/osps/quality/steps.go
@@ -1,6 +1,9 @@
 package quality
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/revanite-io/pvtr-github-repo/evaluation_plans/reusable_steps"
 	"github.com/revanite-io/sci/pkg/layer4"
 )
@@ -29,4 +32,96 @@ func insightsListsRepositories(payloadData interface{}, _ map[string]*layer4.Cha
 	}
 
 	return layer4.Failed, "Insights does NOT contains a list of repositories"
+}
+
+func statusChecksAreRequiredByRulesets(payloadData interface{}, _ map[string]*layer4.Change) (result layer4.Result, message string) {
+	data, message := reusable_steps.VerifyPayload(payloadData)
+	if message != "" {
+		return layer4.Unknown, message
+	}
+
+	// get the name of all status checks that were run
+	var statusChecks []string
+	for _, check := range data.Repository.DefaultBranchRef.Target.Commit.AssociatedPullRequests.Nodes {
+		for _, run := range check.StatusCheckRollup.Commit.CheckSuites.Nodes {
+			for _, checkRun := range run.CheckRuns.Nodes {
+				statusChecks = append(statusChecks, checkRun.Name)
+			}
+		}
+	}
+
+	// get the rules that apply to the default branch
+	rules := data.GetRulesets(data.Repository.DefaultBranchRef.Name)
+	if rules == nil || len(rules) == 0 {
+		return layer4.Passed, "No rulesets found for default branch, continuing to evaluate branch protection"
+	}
+
+	// get the name of all required status checks
+	var requiredChecks []string
+	for _, rule := range data.Rulesets {
+		for _, requiredCheck := range rule.Parameters.RequiredChecks {
+			requiredChecks = append(requiredChecks, requiredCheck.Context)
+		}
+	}
+
+	// check whether all executed checks are required
+	missingChecks := []string{}
+	for _, check := range statusChecks {
+		found := false
+		for _, requiredCheck := range requiredChecks {
+			if check == requiredCheck {
+				found = true
+				break
+			}
+		}
+		if !found {
+			missingChecks = append(missingChecks, check)
+		}
+	}
+
+	if len(missingChecks) > 0 {
+		return layer4.Failed, fmt.Sprintf("Some executed status checks are not mandatory but all should be: %s (NOTE: Not continuing to evaluate branch protection: combining requirements in rulesets and branch protection is not recommended)", strings.Join(missingChecks, ", "))
+	}
+
+	return layer4.Passed, "No status checks were run that are not required by the rules"
+}
+
+func statusChecksAreRequiredByBranchProtection(payloadData interface{}, _ map[string]*layer4.Change) (result layer4.Result, message string) {
+	data, message := reusable_steps.VerifyPayload(payloadData)
+	if message != "" {
+		return layer4.Unknown, message
+	}
+
+	// get the name of all status checks that were run
+	var statusChecks []string
+	for _, check := range data.Repository.DefaultBranchRef.Target.Commit.AssociatedPullRequests.Nodes {
+		for _, run := range check.StatusCheckRollup.Commit.CheckSuites.Nodes {
+			for _, checkRun := range run.CheckRuns.Nodes {
+				statusChecks = append(statusChecks, checkRun.Name)
+			}
+		}
+	}
+
+	requiredChecks := data.Repository.DefaultBranchRef.BranchProtectionRule.RequiredStatusCheckContexts
+
+	// check whether all executed checks are required
+	missingChecks := []string{}
+	for _, check := range statusChecks {
+		found := false
+		for _, requiredCheck := range requiredChecks {
+			if check == requiredCheck {
+				found = true
+				break
+			}
+		}
+		if !found {
+			missingChecks = append(missingChecks, check)
+		}
+	}
+
+	if len(missingChecks) > 0 {
+		return layer4.Failed, fmt.Sprintf("Some executed status checks are not mandatory but all should be: %s", strings.Join(missingChecks, ", "))
+	}
+
+	return layer4.Passed, "No status checks were run that are not required by branch protection"
 }
