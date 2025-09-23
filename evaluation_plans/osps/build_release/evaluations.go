b8df27e3dd5618a729b11844c4016d6ea1c3a167
Travis Truman <trumant@gmail.com>
2025-09-23 09:41:02 -0400

    fix: remove code check dependency for ci check (#144)
    This change closes #143 by updating the evaluation logic to remove any check
    on the contents of the repository beyond those contents in `.github/workflows`
    
    This is a bit of subtle bug in that the control's result remains the same, but
    the underlying reason changes when the control is run against a repository that
    has no workflows in `.github/workflows`
    
    This change was tested and produced the result:
    
    ```yaml
    - requirement-id: OSPS-BR-01.01
      applicability:
        - Maturity Level 1
        - Maturity Level 2
        - Maturity Level 3
      description: When a CI/CD pipeline accepts an input parameter, that parameter MUST be sanitized and validated prior to use in the pipeline.
      result: Passed
      message: "content not found at .github/workflows: directory '.github' not found in path '.github/workflows'"
      steps:
        - github.com/revanite-io/pvtr-github-repo/evaluation_plans/osps/build_release.cicdSanitizedInputParameters
      steps-executed: 1
    ```
    
    Signed-off-by: Travis Truman <trumant@gmail.com>

--- evaluation_plans/osps/build_release/evaluations.go
+++ evaluation_plans/osps/build_release/evaluations.go
@@ -22,7 +22,6 @@ func OSPS_BR_01() (evaluation *layer4.ControlEvaluation) {
 			"Maturity Level 3",
 		},
 		[]layer4.AssessmentStep{
-			reusable_steps.IsCodeRepo,
 			cicdSanitizedInputParameters,
 		},
 	)
